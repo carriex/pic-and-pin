2288cc7a9bc4e6b9a68ad474efc90e90091e5ef2
carrie <carrie.xfy@gmail.com>
2020-04-24 11:49:13 -0500

    Add function for fetch hotel info and map rendering

--- App.js
+++ App.js
@@ -15,14 +15,11 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { v4 as uuidv4 } from 'uuid';
 import * as firebase from 'firebase';
-import { Container, Text, Button, Header, Left, Body, Right, Icon, Title, Content } from 'native-base';
+import { Container, Text, Button, Header, Left, Body, Right, Icon, Title } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import SimpleMap from './simpleMap'
-// import Axios from 'axios';
-// import { Map, GoogleApiWrapper } from 'google-maps-react';
-// import Environment from './config/environment';
-// import firebase from './config/firebase';
+import Axios from 'axios';
 
 const Environment = {
   FIREBASE_API_KEY: 'AIzaSyAmmtXzyytzo_5YuV1NVRvOx2QsHr2lpvo',
@@ -31,19 +28,25 @@ const Environment = {
   FIREBASE_PROJECT_ID: 'pic-and-pin',
   FIREBASE_STORAGE_BUCKET: 'pic-and-pin.appspot.com',
   FIREBASE_MESSAGING_SENDER_ID: '240232545232',
-  GOOGLE_CLOUD_VISION_API_KEY: 'AIzaSyA1Ek0Sj0m3llWAGQCI6bAmOL4x8FO64e4'
+  GOOGLE_CLOUD_VISION_API_KEY: 'AIzaSyA1Ek0Sj0m3llWAGQCI6bAmOL4x8FO64e4',
+  PRICELINE_SERVER: "https://api-sandbox.rezserver.com/api/hotel/getExpress.Results",
+  PRICELINE_REFID: '1346',
+  PRICELINE_APIKEY: '21e1fb57679db2489ba1c9f8c2c79e8c'
+
 };
-if (!firebase.apps.length){
-firebase.initializeApp({
-  apiKey: Environment['FIREBASE_API_KEY'],
-  authDomain: Environment['FIREBASE_AUTH_DOMAIN'],
-  databaseURL: Environment['FIREBASE_DATABASE_URL'],
-  projectId: Environment['FIREBASE_PROJECT_ID'],
-  storageBucket: Environment['FIREBASE_STORAGE_BUCKET'],
-  messagingSenderId: Environment['FIREBASE_MESSAGING_SENDER_ID'],
-  appId: "1:240232545232:web:ae295bead95956b8bed7d5",
-  measurementId: "G-VVN9BF9ZKQ"
-});}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: Environment['FIREBASE_API_KEY'],
+    authDomain: Environment['FIREBASE_AUTH_DOMAIN'],
+    databaseURL: Environment['FIREBASE_DATABASE_URL'],
+    projectId: Environment['FIREBASE_PROJECT_ID'],
+    storageBucket: Environment['FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: Environment['FIREBASE_MESSAGING_SENDER_ID'],
+    appId: "1:240232545232:web:ae295bead95956b8bed7d5",
+    measurementId: "G-VVN9BF9ZKQ"
+  });
+}
 
 
 
@@ -53,13 +56,14 @@ export default class App extends React.Component {
     uploading: false,
     googleResponse: null
   };
+  
 
 
   async componentDidMount() {
-    document.title = "Pic and Pin"
+    document.title = "Pic and Pin";
     await Permissions.askAsync(Permissions.CAMERA_ROLL);
     await Permissions.askAsync(Permissions.CAMERA);
-    
+
     await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
@@ -80,12 +84,12 @@ export default class App extends React.Component {
               <Left>
               </Left>
               <Body>
-                <Title style={{ textAlign:"center" }}>Pic and Pin</Title>
+                <Title style={{ textAlign: "center" }}>Pic and Pin</Title>
               </Body>
               <Right>
-              <Button transparent>
-                <Icon name='menu' />
-              </Button>
+                <Button transparent>
+                  <Icon name='menu' />
+                </Button>
               </Right>
             </Header>
 
@@ -95,8 +99,7 @@ export default class App extends React.Component {
             <Button rounded info onPress={this._pickImage}>
               <Text> Pick an image from camera roll </Text>
             </Button>
-   
-            {/* <Button onPress={this._takePhoto} title="Take a photo" /> */}
+
             {this.state.googleResponse && (
               <FlatList
                 data={this.state.googleResponse.responses[0].labelAnnotations}
@@ -142,32 +145,23 @@ export default class App extends React.Component {
     }
   };
 
+  _maybeRenderHotel = () => {
+    return(
+      <FlatList
+      data={this.state.hotels}
+      renderItem={({ item }) => <Container> 
+      <Text> <Icon name='home'/>{item.name}</Text></Container>}
+      keyExtractor={item => item.id}
+    />
+    )
+  }
+
   _maybeRenderImage = () => {
-    let { image, googleResponse } = this.state;
+    let { image, googleResponse, hotels } = this.state;
     if (!image) {
       return;
     }
-
-  //   if (googleResponse){
-  //   let lag =  this.state.googleResponse.responses[0].landmarkAnnotations[0].locations[0].latLng["latitude"];
-  //   let lon =  this.state.googleResponse.responses[0].landmarkAnnotations[0].locations[0].latLng["longitude"];
-  //   console.log(lag, lon);
-  //   Axios.get('https://api-sandbox.rezserver.com/api/hotel/getExpress.Results',
-  //   { params: {refid: '1346',
-  //   api_key: '21e1fb57679db2489ba1c9f8c2c79e8c',
-  //   format: 'json2',
-  //   latitude: lag,
-  //   longitude: lon,
-  //   check_in: '2020-05-01',
-  //   check_out: '2020-05-03'
-  // }}).then(function(response){
-  //     console.log(response);
-  //   }).catch(function (error) {
-  //     // handle error
-  //     console.log(error);
-  //   });
-  // }
-
+  
 
     return (
       <View
@@ -192,30 +186,35 @@ export default class App extends React.Component {
         >
           <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
         </View>
+
         <Text
           onPress={this._copyToClipboard}
           onLongPress={this._share}
           style={{ paddingVertical: 10, paddingHorizontal: 10 }}
         />
-        
-        <Button rounded info onPress={this.submitToGoogle} style={{ textAlign:"center" }}>
+
+        <Button rounded info onPress={this.submitToGoogle} style={{ textAlign: "center" }}>
           <Text>Locate!</Text>
         </Button>
 
 
-        {googleResponse && 
-          (<SimpleMap center={{ lat: this.state.googleResponse.responses[0].landmarkAnnotations[0].locations[0].latLng['latitude'],
-                                          lng: this.state.googleResponse.responses[0].landmarkAnnotations[0].locations[0].latLng['longitude']}}
-                      description={this.state.googleResponse.responses[0].landmarkAnnotations[0].description}
-/>)}
+        {googleResponse &&
+          (<SimpleMap center={{
+            lat: this.state.googleResponse.responses[0].landmarkAnnotations[0].locations[0].latLng['latitude'],
+            lng: this.state.googleResponse.responses[0].landmarkAnnotations[0].locations[0].latLng['longitude']
+          }}
+            description={this.state.googleResponse.responses[0].landmarkAnnotations[0].description}
+          />)} 
 
 
-{googleResponse && 
-          (<Button rounded info  style={{ textAlign:"center" }}>
-          <Text>Share to Instagram</Text>
-        </Button>)}
+        {googleResponse &&
+          (<Button rounded info onPress={this.shareToInstagram} style={{ textAlign: "center" }}>
+            <Text>Share to Instagram</Text>
+          </Button>)}
 
-        
+        {hotels && this._maybeRenderHotel()}
+
+
       </View>
     );
   };
@@ -239,14 +238,7 @@ export default class App extends React.Component {
     alert('Copied to clipboard');
   };
 
-  _takePhoto = async () => {
-    let pickerResult = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [4, 3]
-    });
 
-    this._handleImagePicked(pickerResult);
-  };
 
   _pickImage = async () => {
     let pickerResult = await ImagePicker.launchImageLibraryAsync({
@@ -303,7 +295,8 @@ export default class App extends React.Component {
           body: body
         }
       );
-      let responseJson = await response.json();
+
+    var responseJson = await response.json();
       this.setState({
         googleResponse: responseJson,
         uploading: false
@@ -311,8 +304,32 @@ export default class App extends React.Component {
     } catch (error) {
       console.log(error);
     }
+
+    try{
+    // get hotel info with the lag and long
+    console.log('Fetching hotel info...')
+    var priceline_response = await getHotelInfoAsync(responseJson);
+
+    console.log(priceline_response);
+    this.setState({
+      hotels: priceline_response['getHotelExpress.Results']['results']['hotel_data']
+
+    });}catch(error){
+      console.log(error);
+    }
+
   };
+
+  shareToInstagram = async () => {
+    //function to share picture to instagram 
+
+  };
+
 }
+
+/****************************
+*     helper functions      *
+*****************************/
 
 async function uploadImageAsync(uri) {
   const blob = await new Promise((resolve, reject) => {
@@ -333,17 +350,63 @@ async function uploadImageAsync(uri) {
   const ref = firebase
     .storage()
     .ref()
-  //.child(uuidv4());
-  .child(Math.floor(Math.random() * 100000).toString());
+    //.child(uuidv4());
+    .child(Math.floor(Math.random() * 100000).toString());
 
   // upload to firebase 
   const snapshot = await ref.put(blob);
 
-  // const url = await snapshot.ref.getDownloadURL();
-  // console.log(url);
-  // return url;
+  // return url 
   return await snapshot.ref.getDownloadURL();
 }
+
+function convert_date(date){
+  var dd = String(date.getDate()).padStart(2, '0');
+  var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = date.getFullYear();
+  date = yyyy + '-' + mm + '-' + dd;
+  return date 
+}
+
+ async function getHotelInfoAsync(response) {
+  // send request to priceline and get response 
+  const lag = response.responses[0].landmarkAnnotations[0].locations[0].latLng["latitude"];
+  const lon = response.responses[0].landmarkAnnotations[0].locations[0].latLng["longitude"];
+  var checkin = new Date();
+  var checkout = new Date(checkin);
+  checkout.setDate(checkin.getDate() + 2);
+  checkin = convert_date(checkin);
+  checkout = convert_date(checkout);
+  console.log(checkin, checkout);
+  console.log(lag, lon);
+
+  var url = new URL(Environment['PRICELINE_SERVER']),
+  // to by pass cors error: https://stackoverflow.com/questions/43262121/trying-to-use-fetch-and-pass-in-mode-no-cors/43268098
+  proxyUrl = 'https://cors-anywhere.herokuapp.com/', 
+  params = {
+    refid: Environment['PRICELINE_REFID'],
+    api_key: Environment['PRICELINE_APIKEY'],
+    format: 'json2',
+    sort_by: 'gs',
+    limit: 10,
+    latitude: lag,
+    longitude: lon,
+    check_in: checkin,
+    check_out: checkout
+  };
+
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+
+  let res = await fetch(proxyUrl + url);
+
+  return await res.json();
+}
+
+/****************************
+*     style sheets          *
+*****************************/
+
 
 const styles = StyleSheet.create({
   container: {
